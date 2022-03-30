88e99061427ac9e9a3baf902046b93fe702ac90d
Shurumov <aleksey.shurumov@aliexpress.ru>
2022-03-30 22:11:31 +0300

    исправлены замечания ревью

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,4 @@
 import ReactDOM from 'react-dom';
 import { App } from './app';
-import { ErrorBoundary } from '/src/components/error-boundary';
 
-ReactDOM.render(
-  <ErrorBoundary>
-    <App />
-  </ErrorBoundary>,
-  document.getElementById('root'),
-);
+ReactDOM.render(<App />, document.getElementById('root'));
