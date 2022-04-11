ffed1eec5c55e7749ff3b00763d357ad1fafbc81
kv <kv@kode.ru>
2022-04-11 21:44:02 +0200

    feat(api): add base instance & auth methods

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { instanceApi } from './api';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [instanceApi.reducerPath]: instanceApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(instanceApi.middleware),
 });
