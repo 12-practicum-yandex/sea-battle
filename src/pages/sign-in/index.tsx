c9d817066644704d9336dd7297b868085ca9f82f
kv <kv@kode.ru>
2022-04-13 22:57:51 +0200

    feat(auth): add redirect after login success

--- src/pages/sign-in/index.tsx
+++ src/pages/sign-in/index.tsx
@@ -2,11 +2,18 @@ import { useSignInMutation } from '@api/auth';
 import { PageLayout } from '@layouts';
 import { SignInForm } from '@components';
 import { TSignInFormValues } from '@components/sign-in-form/types';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '@constants/routes';
 
 export const SignIn = () => {
+  const navigate = useNavigate();
+
   const [signInMutation, { isLoading: isSignInMutationLoading }] = useSignInMutation();
 
-  const onSubmit = (values: TSignInFormValues) => signInMutation(values);
+  const onSubmit = (values: TSignInFormValues) =>
+    signInMutation(values)
+      .unwrap()
+      .then(() => navigate(ROUTES.INIT_GAME));
 
   return (
     <PageLayout isCenter>
