4e57580375a362a9714f251d38d44400d6244bd9
kv <kv@kode.ru>
2022-04-13 22:18:24 +0200

    feat(auth): handle sign up

--- src/pages/sign-up-page/index.tsx
+++ src/pages/sign-up-page/index.tsx
@@ -1,16 +1,30 @@
 import { PageLayout } from '@layouts';
 import { ProfileForm } from '@components';
+import { useSignUpMutation } from '@api/auth';
+import { TSignUpFormValues } from '@components/profile-form/types';
+import { useNavigate } from 'react-router-dom';
+import { ROUTES } from '@constants/routes';
 
 export const SignUpPage = () => {
-  const onSubmit = (values: any) => {
-    console.log(values);
+  const navigate = useNavigate();
 
-    return Promise.resolve();
-  };
+  const [signUpMutation, { isLoading }] = useSignUpMutation();
+
+  const onSubmit = (values: TSignUpFormValues) =>
+    signUpMutation({
+      email: values.email,
+      first_name: values['first-name'],
+      login: values.login,
+      password: values.password,
+      phone: values.phone,
+      second_name: values['second-name'],
+    })
+      .unwrap()
+      .then(() => navigate(ROUTES.INIT_GAME));
 
   return (
     <PageLayout isCenter>
-      <ProfileForm onSubmit={onSubmit} isLoading={false} />
+      <ProfileForm onSubmit={onSubmit} isLoading={isLoading} />
     </PageLayout>
   );
 };
