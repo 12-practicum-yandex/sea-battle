75922310ab2f65e08223cefda214837942e2da34
safokhin <84101026+safokhin@users.noreply.github.com>
2022-05-23 18:11:26 +0300

    add steps game (#23)

--- src/pages/game/index.tsx
+++ src/pages/game/index.tsx
@@ -6,20 +6,26 @@ import { GameSettings } from '@features/gameSea/hooks/GameSettings';
 import { Header } from '@components';
 import { BoardsWrapper, GameWrapper } from '@pages/game/styles';
 import { Footer } from '@pages/game/Footer';
+import { CellType } from '@features/canvas/game-cell/types';
 import { useNotification } from '@features/use-notification';
 
 export type GameType = { board: BoardType | null; ships: ShipProps[] };
-export type CallbackBoardType = (board: BoardType, isMeBoard: boolean) => void;
+export type CallbackBoardType = (board: BoardType, isMeBoard: boolean, cellType?: CellType) => void;
 export type CallbackShipsType = (ships: ShipProps[], isMeShips: boolean) => void;
 
 export const Game = () => {
+  const [isMeStep, setIsMeStep] = useState<boolean>(true);
   const { createNotification } = useNotification();
-
   const [myGame, setMyGame] = useState<GameType>({ board: null, ships: [] }); // Данные для нашей игры
   const [enemyGame, setEnemyGame] = useState<GameType>({ board: null, ships: [] }); // Данные для игры соперника
   const [typeGame, setTypeGame] = useState<TypeGame | null>(null);
   // Возвращает матрицу после любых изменений (попадание по кораблю, расстановка кораблей и т.д.)
-  const callbackBoard: CallbackBoardType = (board, isMeBoard) => {
+  const callbackBoard: CallbackBoardType = (board, isMeBoard, cellType) => {
+    // Передача хода
+    if (cellType === CellType.miss) {
+      setIsMeStep((prev) => !prev);
+    }
+
     if (isMeBoard) {
       setMyGame((prev) => ({ ...prev, board }));
     } else {
@@ -48,7 +54,12 @@ export const Game = () => {
     const myShips = myGame.ships.filter((ship) => ship.isPositionCell !== null);
     const enemyShips = enemyGame.ships.filter((ship) => ship.isPositionCell !== null);
 
-    if (myShips.length === 10 && enemyShips.length === 10) {
+    if (myShips.length === 10) {
+      setIsMeStep(false); // Я расставил корабли. Теперь расставляет противник
+    }
+
+    if (enemyShips.length === 10) {
+      setIsMeStep(true); // Противник расставил корабли, теперь я хожу
       setTypeGame(TypeGame.battle);
     }
   };
@@ -73,6 +84,7 @@ export const Game = () => {
   return (
     <GameWrapper>
       <Header />
+      <span>{isMeStep ? 'Ваш ход' : 'Ход противника'}</span>
       <BoardsWrapper>
         {myGame.board !== null && (
           <GameSettings
@@ -83,6 +95,7 @@ export const Game = () => {
             callbackDeadShip={deadShipHandler}
             typeGame={typeGame}
             isMe={true}
+            isMeStep={isMeStep}
           />
         )}
         {enemyGame.board !== null &&
@@ -95,6 +108,7 @@ export const Game = () => {
               callbackDeadShip={deadShipHandler}
               typeGame={typeGame}
               isMe={false}
+              isMeStep={!isMeStep}
             />
           )}
       </BoardsWrapper>
