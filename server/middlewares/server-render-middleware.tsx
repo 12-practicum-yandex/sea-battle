49da69fbb4502ed5dbf14716125dcaf7f5b05280
andrey_kv <a.kovalenok679@gmail.com>
2022-05-27 19:59:42 +0300

    feat(ssr): add ssr (#24)
    Co-authored-by: kv <kv@kode.ru>

---
+++ server/middlewares/server-render-middleware.tsx
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express';
+import { renderToString } from 'react-dom/server';
+import { Helmet } from 'react-helmet';
+
+import { StaticRouter } from 'react-router-dom/server';
+
+import { App } from '../../src/app';
+
+const getHtml = (reactHtml: string) => {
+  const helmet = Helmet.renderStatic();
+  return `
+  <!DOCTYPE html>
+  <html ${helmet.htmlAttributes.toString()}>
+    <head>
+      ${helmet.title.toString()}
+      ${helmet.meta.toString()}
+      ${helmet.link.toString()}
+    </head>
+    <body>
+      <div id="root">${reactHtml}</div>
+      <script src="main.js"></script>
+    </body>
+  </html>
+  `;
+};
+
+export const serverRenderMiddleware = (req: Request, res: Response) => {
+  //TODO: add redux store
+  const jsx = (
+    <StaticRouter location={req.url}>
+      <App />
+    </StaticRouter>
+  );
+
+  const reactHtml = renderToString(jsx);
+
+  res.status(200).send(getHtml(reactHtml));
+};
