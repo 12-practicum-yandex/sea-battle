f47fb5a866a67e4694a4b671054b355dfc2dcf5f
kv <kv@kode.ru>
2022-06-06 21:51:03 +0200

    feat(server): init db

---
+++ server/init/index.ts
@@ -0,0 +1,23 @@
+import { Sequelize, SequelizeOptions } from 'sequelize-typescript';
+
+const sequelizeOptions: SequelizeOptions = {
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  dialect: 'postgres', // 'mysql', 'sqlite', 'mariadb', 'mssql'
+};
+
+// Создаем инстанс Sequelize
+export const sequelize = new Sequelize(sequelizeOptions);
+
+export async function dbConnect() {
+  try {
+    await sequelize.authenticate(); // Проверка аутентификации в БД
+    await sequelize.sync(); // Синхронизация базы данных
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
