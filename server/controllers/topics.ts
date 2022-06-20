a742bcd6bf5cad921035b98b796fd336ba0f77fb
andrey_kv <a.kovalenok679@gmail.com>
2022-06-20 21:08:47 +0300

    feat(comments): add comments methods (#36)
    Co-authored-by: kv <kv@kode.ru>

--- server/controllers/topics.ts
+++ server/controllers/topics.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from 'express';
-import { TopicModel, UserModel } from '../models';
+import { TopicModel, UserModel, TopicCommentModel } from '../models';
 
 export const topicsRouter = Router();
 
@@ -27,7 +27,6 @@ const addTopic = async (req: Request, res: Response) => {
     res.send('OK');
   } catch (error) {
     console.log(error);
-
     res.status(400).send();
   }
 };
@@ -39,9 +38,75 @@ const getAll = async (req: Request, res: Response) => {
       topics: data,
     });
   } catch (error) {
+    console.log(error);
+    res.status(400).send();
+  }
+};
+
+const createComment = async (req: Request, res: Response) => {
+  try {
+    const { body } = req;
+    const { parentId, comment, topicId, userId, userLogin } = body;
+
+    await UserModel.findOrCreate({
+      where: {
+        id: userId,
+      },
+      defaults: {
+        id: userId,
+        login: userLogin,
+      },
+    });
+
+    await TopicCommentModel.create({
+      comment,
+      topic_id: topicId,
+      user_id: userId,
+      parent_id: parentId,
+    });
+
+    res.send('OK');
+  } catch (error) {
+    console.log(error);
+    res.status(400).send();
+  }
+};
+
+const deleteComment = async (req: Request, res: Response) => {
+  try {
+    const { body } = req;
+    const { id } = body;
+
+    const comment = await TopicCommentModel.findByPk(id);
+    await comment?.destroy();
+
+    res.send('OK');
+  } catch (error) {
+    console.log(error);
+    res.status(400).send();
+  }
+};
+
+const getComment = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params;
+
+    const data = await TopicCommentModel.findAll({
+      where: {
+        topic_id: id,
+      },
+    });
+    res.send({
+      comments: data,
+    });
+  } catch (error) {
+    console.log(error);
     res.status(400).send();
   }
 };
 
 topicsRouter.route('/add').post(addTopic);
 topicsRouter.route('/all').get(getAll);
+topicsRouter.route('/add-comment').post(createComment);
+topicsRouter.route('/delete-comment').delete(deleteComment);
+topicsRouter.route('/get-comments').get(getComment);
