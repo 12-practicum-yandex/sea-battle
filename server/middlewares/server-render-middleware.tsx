d9e665eabb50626e7b5ef2722c5cc7e1708a9394
safokhin <84101026+safokhin@users.noreply.github.com>
2022-06-21 11:07:01 +0300

    init ssr redux-toolkit (#38)

--- server/middlewares/server-render-middleware.tsx
+++ server/middlewares/server-render-middleware.tsx
@@ -5,8 +5,12 @@ import { Helmet } from 'react-helmet';
 import { StaticRouter } from 'react-router-dom/server';
 
 import { App } from '../../src/app';
+import { Provider } from 'react-redux';
+import { create } from '../../src/store';
 
-const getHtml = (reactHtml: string, scriptPath: string) => {
+type GetHtmlProps = { reactHtml: string; scriptPath: string; preloadedState: string };
+
+const getHtml = ({ reactHtml, scriptPath, preloadedState }: GetHtmlProps) => {
   const helmet = Helmet.renderStatic();
   return `
   <!DOCTYPE html>
@@ -17,6 +21,7 @@ const getHtml = (reactHtml: string, scriptPath: string) => {
       ${helmet.link.toString()}
     </head>
     <body>
+      <script>window.__PRELOADED_STATE__=${preloadedState}</script>
       <div id="root">${reactHtml}</div>
       <script src="${scriptPath}"></script>
     </body>
@@ -27,16 +32,20 @@ const getHtml = (reactHtml: string, scriptPath: string) => {
 export const serverRenderMiddleware = (req: Request, res: Response) => {
   const { devMiddleware } = res.locals.webpack;
   const { assetsByChunkName } = devMiddleware.stats.toJson();
-  const script = assetsByChunkName.main[0];
+  const scriptPath = assetsByChunkName.main[0];
 
-  //TODO: add redux store
+  const store = create({});
+
   const jsx = (
     <StaticRouter location={req.url}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </StaticRouter>
   );
 
+  const preloadedState = JSON.stringify(store.getState()).replace(/</g, '\\u003c');
   const reactHtml = renderToString(jsx);
 
-  res.status(200).send(getHtml(reactHtml, script));
+  res.status(200).send(getHtml({ reactHtml, scriptPath, preloadedState }));
 };
--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,9 @@
-import { store } from './store';
+import { store } from './index';
 
 declare global {
+  interface Window {
+    __PRELOADED_STATE__?: any;
+  }
   type GlobalStore = ReturnType<typeof store.getState>;
   type GlobalDispatch = typeof store.dispatch;
 }
